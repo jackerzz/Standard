95a0099f1c4daf333b7319b96f77436ea1e2729d
jackerzz <1101694369@qq.com>
2021-07-21 23:02:04 +0800

    up20

--- middleware/secureHttpsMiddleware.py
+++ middleware/secureHttpsMiddleware.py
@@ -1,10 +1,12 @@
-import json
+import json,time
 from starlette.requests import Request
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
 from starlette.datastructures import MutableHeaders
-from SecureHTTP import EncryptedCommunicationServer
+from SecureHTTP import RSADecrypt,AESDecrypt,AESEncrypt
 from db.session import redis_session
+from libs.snowflakeAlgorithm import IdWorker
 
+worker = IdWorker(1, 2, 0)
 
 class MessageSecureHTTPMiddleware:
     def __init__(self, app: ASGIApp) -> None:
@@ -27,9 +29,8 @@ class _MessageSecureHTTPResponder:
         self.method = str
         self.initial_message: Message = {}
         self.started = False
-        self.client = redis_session()
-        self.resServer = EncryptedCommunicationServer(
-            self.client.get('privkey'))
+        self.privkey = redis_session().get('privkey')
+        self.asekey = ''
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         self.headers = MutableHeaders(scope=scope)
@@ -45,11 +46,15 @@ class _MessageSecureHTTPResponder:
             解密返回
         '''
         message = await self.receive()
-        print("解密返回原生message:", message)
+        if self.url in ['/docs', '/api/v1/openapi.json']:
+            await self.send(message)
+            return
+
         body = json.loads(str(message['body'], encoding="utf-8"))
-        body = self.resServer.serverDecrypt(body)
+        self.asekey = RSADecrypt(self.privkey,body['key'])
+        body = AESDecrypt(self.asekey,body['value'])
+
         message['body'] = bytes(json.dumps(body), encoding='utf-8')
-        print('解密成功message：', message)
         return message
 
     async def send_with_msg(self, message: Message) -> None:
@@ -62,26 +67,23 @@ class _MessageSecureHTTPResponder:
 
         if message["type"] == "http.response.start":
             self.initial_message = message
-            # await self.send(message)
             return
 
         elif message["type"] == "http.response.body":
-            print("加密返回原生http.response.body --> message:", message)
-            print("加密返回原生http.response.start --> message:", self.initial_message)
             headers = MutableHeaders(raw=self.initial_message['headers'])
-            # bytes to dict => dict to bytes
+
+
             body = json.loads(str(message['body'], encoding="utf-8"))
-            body = self.resServer.serverEncrypt(body)
+            body = dict(data=AESEncrypt(self.asekey, json.dumps(body, separators=(',', ':')), output_type="str"))
             body = bytes(json.dumps(body), encoding='utf-8')
 
             # 更新 body
             message["body"] = body
-            print("更新 body ", body)
 
             # 自定义响应header
             headers["Content-Length"] = str(len(body))
-            headers['ac'] = "hhhh"
-            headers.append("Set-Cookie", "fasdfdsa")
+            headers['timestamp'] = round(time.time())
+            headers['nonce'] = worker.get_id()
 
             self.initial_message['headers'] = headers._list
             await self.send(self.initial_message)
